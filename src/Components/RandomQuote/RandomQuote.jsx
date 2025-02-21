da0a9eeab712a58c08187bab3afb300828bc67ac
Tharun <tharungowda30115@gmail.com>
2025-02-21 23:39:36 +0530

    minor changes

--- src/Components/RandomQuote/RandomQuote.jsx
+++ src/Components/RandomQuote/RandomQuote.jsx
@@ -3,10 +3,23 @@ import './RandomQuote.css'
 
  const RandomQuote = () => {
 
+    let quotes = [];
+
+    async function loadQuotes() {
+        const response = await fetch("https://type.fit/api/quotes")
+        quotes = await response.json();
+    }
+
+    const random = () => {
+        const select = quotes[Math.floor(Math.random()*quotes.length)]
+    }
+
     const [quote,setQuote] = useState({
         text: "Difficulties increase the nearer we get to the goal",
         author: "Johann Wolfgang von Goethe",
     })
+
+    loadQuotes();
 
   return (
     <div className='container'>
