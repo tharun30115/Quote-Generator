072950ac312ff134c6f6d0ac6b414983faf94bc9
Tharun <tharungowda30115@gmail.com>
2025-02-21 23:56:27 +0530

    minor changes

--- src/Components/RandomQuote/RandomQuote.jsx
+++ src/Components/RandomQuote/RandomQuote.jsx
@@ -1,41 +1,46 @@
-import React, { useState } from 'react'
-import './RandomQuote.css'
+import React, { useState } from "react";
+import "./RandomQuote.css";
 
- const RandomQuote = () => {
+const RandomQuote = () => {
+  let quotes = [];
 
-    let quotes = [];
+  async function loadQuotes() {
+    const response = await fetch("https://quotes-api-self.vercel.app/quote");
+    quotes = await response.json();
+  }
 
-    async function loadQuotes() {
-        const response = await fetch("https://type.fit/api/quotes")
-        quotes = await response.json();
-    }
+  const [quote,setQuote] = useState({
+    text: "Difficulties increase the nearer we get to the goal",
+    author:"Johann Wolfgang von Goethe",
+  });
 
-    const random = () => {
-        const select = quotes[Math.floor(Math.random()*quotes.length)]
-    }
+  const random = () => {
+    const select = quotes[Math.floor(Math.random() * quotes.length)];
+    setQuote(select);
+  };
 
-    const [quote,setQuote] = useState({
-        text: "Difficulties increase the nearer we get to the goal",
-        author: "Johann Wolfgang von Goethe",
-    })
-
-    loadQuotes();
+  loadQuotes()
 
   return (
-    <div className='container'>
-        <div className='quote'>{quote.text}</div>
-        <div>
-            <div className='line'></div>
-            <div className='bottom'>
-                <div className="author">{quote.author}</div>
-                <div className="icons">
-                    <i class='bx bx-ghost bx-md'></i>
-                    <i class='bx bxl-twitter bx-md'></i>
-                </div>
-            </div>
+    <div className="container">
+      <div className="quote">{quote.text}</div>
+      <div>
+        <div className="line"></div>
+        <div className="bottom">
+          <div className="author">{quote.author}</div>
+          <div className="icons">
+            <i
+              class="bx bx-ghost bx-md"
+              onClick={() => {
+                random()
+              }}
+            ></i>
+            <i class="bx bxl-twitter bx-md"></i>
+          </div>
         </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default RandomQuote
+export default RandomQuote;
